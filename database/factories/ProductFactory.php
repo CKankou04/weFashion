177634ae2ef08d3e5479ba19ccf6ca783de28b49
kankou <kconte@enovalp.com>
2022-06-21 19:13:57 +0200

    Creation Migration Model et Factory

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Product>
@@ -17,7 +18,13 @@ class ProductFactory extends Factory
     public function definition()
     {
         return [
-            //
+            //creons un titre(6mots par defauts) et un paragraphe(3par defaut)
+            'name' => $this->faker->sentence(),
+            'description' => $this->faker->paragraph(),
+            'price' => $this->faker->randomFloat(2, 5, 2000),
+            'stateProduct' => $this->faker->randomElement(['sale', 'standard']),
+            'isVisible' => $this->faker->randomElement(['publish', 'unpublish']),
+            'referenceProduct' => Str::random(16)
         ];
     }
 }
